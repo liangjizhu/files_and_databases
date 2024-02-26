34163261c3c01019130a9d9986ba1219901419c9
Liang Ji <100495723@alumnos.uc3m.es>
2024-02-26 15:38:28 +0100

    1

--- lab1.sql
+++ lab1.sql
@@ -1,5 +1,5 @@
 DROP TABLE catalogue;
-DROP TABLE product;
+DROP TABLE products;
 DROP TABLE marketing_format;
 DROP TABLE p_reference;
 DROP TABLE replacement_order;
@@ -17,6 +17,24 @@ DROP TABLE comments;
 
 
 CREATE TABLE catalogue{
+    product VARCHAR(50) NOT NULL,
+    CONSTRAINT pk_catalogue PRIMARY KEY(product)
+};
+
+CREATE TABLE products{
+    -- product_id == name
+    product_id VARCHAR(50) NOT NULL,
+    coffea VARCHAR(20),
+    varietal VARCHAR(30),
+    origin VARCHAR(15),
+    roast_type VARCHAR(10),
+    decaff VARCHAR(12),
+    -- marketing_format is a code for identifying what packaging a product follows
+    marketing_format VARCHAR(2),
+    p_reference VARCHAR(15),
+
+    CONSTRAINT pk_catalogue PRIMARY KEY(product)
+
     
-}
+};
 
