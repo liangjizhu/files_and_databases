7164643af0e9bfa4f7cadbc97d318122611a6fd2
Liang Ji <100495723@alumnos.uc3m.es>
2024-02-26 15:17:17 +0100

    lab1

---
+++ lab1.sql
@@ -0,0 +1,22 @@
+DROP TABLE catalogue;
+DROP TABLE product;
+DROP TABLE marketing_format;
+DROP TABLE p_reference;
+DROP TABLE replacement_order;
+DROP TABLE supplier;
+DROP TABLE supply_line;
+DROP TABLE purchase_order;
+DROP TABLE billing_data;
+DROP TABLE credit_card_data;
+DROP TABLE delivery;
+DROP TABLE delivery_data;
+DROP TABLE registered;
+DROP TABLE non_registered;
+DROP TABLE client_feedback;
+DROP TABLE comments;
+
+
+CREATE TABLE catalogue{
+    
+}
+
