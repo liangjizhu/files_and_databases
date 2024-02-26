e91ff0a7b5eb059c41b08f843958a4518c79c647
Liang Ji <100495723@alumnos.uc3m.es>
2024-02-26 16:44:10 +0100

    datatype

--- lab1.sql
+++ lab1.sql
@@ -1,6 +1,5 @@
 DROP TABLE catalogue;
 DROP TABLE products;
-DROP TABLE marketing_format;
 DROP TABLE p_reference;
 DROP TABLE replacement_order;
 DROP TABLE supplier;
@@ -28,13 +27,32 @@ CREATE TABLE products{
     varietal VARCHAR(30),
     origin VARCHAR(15),
     roast_type VARCHAR(10),
-    decaff VARCHAR(12),
-    -- marketing_format is a code for identifying what packaging a product follows
-    marketing_format VARCHAR(2),
+    decaff BOOL,
+    -- if NULL -> delete product
+    packaging VARCHAR(15),
     p_reference VARCHAR(15),
-
-    CONSTRAINT pk_catalogue PRIMARY KEY(product)
-
-    
+    CONSTRAINT pk_products PRIMARY KEY(product_id, p_reference),
+    CONSTRAINT fk_products_catalogue FOREIGN KEY(product_id) REFERENCES catalogue(product)
 };
+
+CREATE TABLE p_reference{
+    bar_code VARCHAR(15),
+    packaging VARCHAR(15),
+    retail_price VARCHAR(14),
+    min_stock INT CHECK (min_stock >= 0),
+    max_stock INT CHECK (max_stock >= min_stock),
+    current_stock INT CHECK (max_stock >= current_stock >= min_stock),
+    CONSTRAINT pk_p_reference PRIMARY KEY(bar_code),
+    CONSTRAINT fk_p_reference_products FOREIGN KEY(bar_code) REFERENCE products(p_reference)
+};
+
+CREATE TABLE replacement_order{
+    p_reference VARCHAR(15);
+    supplier VARCHAR(35);
+    request_amount VARCHAR(2);
+    request_date DATE NOT NULL;
+    delivery_date DATE NOT NULL;
+
+}
+
 
