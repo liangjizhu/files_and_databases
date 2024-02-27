9bddc239124cd629a4fedc29ee512d8c4a5c0280
Liang Ji Zhu <www.1159068228@gmail.com>
2024-02-27 02:10:50 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- lab1.sql
+++ lab1.sql
@@ -11,7 +11,7 @@ DROP TABLE orders_item;
 DROP TABLE customers;
 DROP TABLE registered;
 DROP TABLE non_registered;
-DROP TABLE client_feedback;
+DROP TABLE customer_feedback;
 DROP TABLE comments;
 
 
@@ -23,27 +23,29 @@ CREATE TABLE catalogue (
 CREATE TABLE products(
     -- product_id == name
     product_id VARCHAR(50) NOT NULL,
-    coffea VARCHAR(20),
-    varietal VARCHAR(30),
-    origin VARCHAR(15),
-    roast_type VARCHAR(10),
-    decaff BOOL,
+    coffea VARCHAR(20) NOT NULL,
+    varietal VARCHAR(30) NOT NULL,
+    origin VARCHAR(15) NOT NULL,
+    roast_type VARCHAR(10) NOT NULL,
+    decaff CHAR(1) NOT NULL,
     -- if NULL -> delete product
-    packaging VARCHAR(15),
-    p_reference VARCHAR(15),
-    CONSTRAINT pk_products PRIMARY KEY(product_id, p_reference),
+    packaging VARCHAR(15) NOT NULL,
+    p_reference VARCHAR(15) NOT NULL,
+    CONSTRAINT pk_products PRIMARY KEY(product_id),
     CONSTRAINT fk_products_catalogue FOREIGN KEY(product_id) REFERENCES catalogue(product)
 );
 
 CREATE TABLE p_reference(
-    bar_code VARCHAR(15),
+    bar_code VARCHAR(15) NOT NULL,
     packaging VARCHAR(15),
     retail_price VARCHAR(14),
     min_stock INT CHECK (min_stock >= 5),
-    max_stock INT CHECK (max_stock >= min_stock),
-    current_stock INT CHECK (max_stock >= current_stock >= min_stock),
+    max_stock INT NOT NULL,
+    current_stock INT NOT NULL,
     CONSTRAINT pk_p_reference PRIMARY KEY(bar_code),
-    CONSTRAINT fk_p_reference_products FOREIGN KEY(bar_code) REFERENCE products(p_reference)
+    CONSTRAINT fk_p_reference_products FOREIGN KEY(bar_code) REFERENCES products(product_id),
+    CONSTRAINT check_current_stock CHECK(current_stock >= min_stock),
+    CONSTRAINT check_max_stock CHECK(max_stock >= min_stock)
 );
 
 CREATE TABLE replacement_order(
@@ -66,8 +68,8 @@ CREATE TABLE supplier(
     provider_name VARCHAR(50) NOT NULL;
     cif VARCHAR(10) NOT NULL;
     full_name VARCHAR(100) NOT NULL;
-    email VARCHAR(100) NOT NULL;
-    phone_number INT CHECK(999999999 > phone_number >= 100000000);
+    supplier_email VARCHAR(100) NOT NULL;
+    supplier_phone_number INT CHECK(999999999 > supplier_phone_number >= 100000000);
     comm_address VARCHAR(100) NOT NULL;
     offer FLOAT CHECK(offer > 0);
     fulfilled_orders INT CHECK(fulfilled_orders >= 0);
@@ -81,20 +83,6 @@ CREATE TABLE purchase_order(
     purchase_date DATE NOT NULL;
     delivery_data VARCHAR(50);    
 );
-CREATE TABLE billing_data(
-    customer_id INT CHECK(customer_id >= 0);
-    -- if bill_type == credit card -> credit_card_data
-    bill_type VARCHAR(20);
-    payment_date DATE NOT NULL;
-    
-);
-
-CREATE TABLE credit_card_data(
-    customer_id INT CHECK(customer_id >= 0);
-    cardholder VARCHAR(50);
-    finance_company VARCHAR(30);
-    card_number INT CHECK(9999999999999999 > credit_card_data >= 1000000000000000)
-);
 
 CREATE TABLE delivery(
     customer_id INT CHECK(customer_id >= 0);
@@ -103,18 +91,74 @@ CREATE TABLE delivery(
 );
 
 CREATE TABLE orders_item(
-    order_id VARCHAR(20);
+    order_id VARCHAR(20) NOT NULL;
+    product_id VARCHAR(50) NOT NULL;
+    quantity INT CHECK(quantity > 0) NOT NULL;
+    unit_price FLOAT CHECK(unit_price > 0) NOT NULL;
+    total_price FLOAT NOT NULL;
+);
+
+
+
+CREATE TABLE customers(
+    customer_id INT CHECK(customer_id >= 0) NOT NULL;
+    delivery_address VARCHAR(100) NOT NULL;
+    billing_data VARCHAR(20) NOT NULL;
+    registered BOOL DEFAULT FALSE;
+    customer_email VARCHAR(100) NOT NULL;
+    customer_phone_number INT CHECK(999999999 > supplier_phone_number >= 100000000);
+);
+
+CREATE TABLE registered(
+    customer_id INT CHECK(customer_id >= 0) NOT NULL;
+    reg_username VARCHAR(30) NOT NULL;
+    reg_password VARCHAR(40) NOT NULL;
+    reg_date DATE NOT NULL;
+    reg_name VARCHAR(30) NOT NULL;
+    reg_surname_1 VARCHAR(30) NOT NULL;    
+    reg_surname_2 VARCHAR(30);
+    contact_preference VARCHAR(30) NOT NULL;
+    loyalty_discount BOOL;
+    order_id VARCHAR(20) NOT NULL;
+);
+
+CREATE TABLE non_registered(
+    order_id VARCHAR(20) NOT NULL;
+    non_reg_name VARCHAR(30) NOT NULL;
+    non_reg_surname VARCHAR(30) NOT NULL;
+);
+
+CREATE TABLE billing_data(
+    customer_id INT CHECK(customer_id >= 0) NOT NULL;
+    -- if bill_type == credit card -> credit_card_data
+    bill_type VARCHAR(20);
+    payment_date DATE NOT NULL;
+    credit_card_data BOOL;
+);
+
+CREATE TABLE credit_card_data(
+    customer_id INT CHECK(customer_id >= 0) NOT NULL;
+    cardholder VARCHAR(50) NOT NULL;
+    finance_company VARCHAR(30) NOT NULL;
+    card_number INT CHECK(9999999999999999 > credit_card_data >= 1000000000000000);
+    expiration_date DATE NOT NULL;
+);
+
+CREATE TABLE customer_feedbacks(
+    customer_id INT CHECK(customer_id >= 0) NOT NULL;
     product_id VARCHAR(50);
-    quantity INT CHECK(quantity > 0);
-    unit_price FLOAT CHECK(unit_price > 0);
-    total_price FLOAT;
+    bar_code VARCHAR(15);
+    opinion VARCHAR(1000);
+    rating INT CHECK(5 >= rating > 0);
+    customer_comment VARCHAR(1000);
+
     
 );
 
-CREATE TABLE customers(
-    customer_id INT CHECK(customer_id >= 0);
-
+CREATE TABLE customer_comments(
+    comment_id VARCHAR(255) NOT NULL;
+    score INT CHECK(10 >= rating > 0);
+    text VARCHAR(1000);
+    likes INT CHECK(likes >= 0);
+    tag VARCHAR(40);
 );
-
-
-
