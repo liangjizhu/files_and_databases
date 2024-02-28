f9ce2c9612bdb3b544d77f692e82e2ab30c61403
Liang Ji <100495723@alumnos.uc3m.es>
2024-02-28 16:47:29 +0100

    12323213

--- commands.sql
+++ commands.sql
@@ -25,7 +25,7 @@ SELECT
     -- product_id
     seq_product_id.NEXTVAL,
     -- product_name
-    DISTINCT c.PRODUCT,
+    DISTINCT PRODUCT,
     COFFEA,
     VARIETAL,
     ORIGIN,
@@ -44,6 +44,46 @@ SELECT
 FROM fsdb.catalogue
 WHERE PRODUCT IS NOT NULL AND DECAF IS NOT NULL AND COFFEA IS NOT NULL
 AND VARIETAL IS NOT NULL AND ORIGIN IS NOT NULL AND ROASTING IN ('natural', 'high-roast', 'blend');
+
+-- 
+-- 
+INSERT INTO products (product_id, product_name, coffea, varietal, origin, roast_type, decaff)
+SELECT
+    seq_product_id.NEXTVAL,
+    sub.PRODUCT,
+    sub.COFFEA,
+    sub.VARIETAL,
+    sub.ORIGIN,
+    CASE
+        WHEN sub.ROASTING = 'blend' THEN 'mixture'
+        WHEN sub.ROASTING = 'natural' THEN 'natural'
+        WHEN sub.ROASTING = 'high-roast' THEN 'high-roast'
+        ELSE sub.ROASTING
+    END,
+    CASE
+        WHEN sub.DECAF = 'yes' THEN 'Y'
+        WHEN sub.DECAF = 'no' THEN 'N'
+        ELSE sub.DECAF
+    END
+FROM (
+    SELECT DISTINCT
+        PRODUCT,
+        COFFEA,
+        VARIETAL,
+        ORIGIN,
+        CASE
+            WHEN ROASTING = 'blend' THEN 'mixture'
+            WHEN ROASTING = 'natural' THEN 'natural'
+            WHEN ROASTING = 'high-roast' THEN 'high-roast'
+        ELSE ROASTING
+        END,
+        DECAF
+    FROM fsdb.catalogue
+    WHERE PRODUCT IS NOT NULL AND DECAF IS NOT NULL AND COFFEA IS NOT NULL
+    AND VARIETAL IS NOT NULL AND ORIGIN IS NOT NULL AND ROASTING IN ('natural', 'high-roast', 'blend')
+) sub;
+-- 
+-- 
 
 select distinct product_id, product_name, coffea, varietal, origin, roast_type, decaff from products;
 
