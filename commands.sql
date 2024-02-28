4baef5c4f673cc2248a1a3f6d3bc42fba79b9cfc
Liang Ji Zhu <www.1159068228@gmail.com>
2024-02-28 01:21:26 +0100

    123

--- commands.sql
+++ commands.sql
@@ -5,10 +5,15 @@ select * from all_tables;
 select * from fsdb.catalogue;
 select * from fsdb.trolley;
 select * from fsdb.posts;
+select * from all_sequences;
 
 -- trying to insert to the "products" table
 select PRODUCT, FORMAT, COFFEA, VARIETAL, ORIGIN, ROASTING, DECAF, PACKAGING from fsdb.catalogue;
 
+INSERT INTO products (column1, column2, ...)
+SELECT column1, column2, ...
+FROM fsdb.catalogue
+WHERE condition; -- Optional condition to filter the data
 
 desc catalogue;
 desc products;
@@ -26,3 +31,4 @@ desc registered;
 desc non_registered;
 desc customer_feedbacks;
 desc customer_comments;
+
