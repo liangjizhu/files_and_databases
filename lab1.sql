f516ab41bad417c2adcd4bb4462e03cd6958ba66
Liang Ji <100495723@alumnos.uc3m.es>
2024-03-01 19:44:18 +0100

    123

--- lab1.sql
+++ lab1.sql
@@ -120,40 +120,13 @@ CREATE TABLE supplier(
 -- START "BUYING"
 CREATE TABLE customers(
     customer_id NUMBER CHECK(customer_id >= 40000) NOT NULL,
---     username CHAR(30),
---     reg_date CHAR(14),
---     reg_time CHAR(14),
---     user_password CHAR(15),
---     customer_name CHAR(35) NOT NULL,
---     customer_surname1 CHAR(30) NOT NULL,
---     customer_surname2 CHAR(30),
---     customer_email VARCHAR(60),
---     customer_phone_number CHAR(9),
     delivery_address VARCHAR(100) NOT NULL,
-    billing_data VARCHAR(20) NOT NULL,
+    billing_id,
     registered CHAR(1),
     customer_email VARCHAR(100) NOT NULL,
     customer_phone_number INT CHECK(customer_phone_number >= 100000000),
     CONSTRAINT pk_customers PRIMARY KEY(customer_id),
     CONSTRAINT check_customer_phone_number CHECK(999999999 > customer_phone_number)
-);
-
-CREATE TABLE address(
-    address_id INT CHECK (address_id >=50000) NOT NULL,
-    add_type VARCHAR(20) NOT NULL,
-    add_name VARCHAR(100) NOT NULL,
-    gateway_num INT,
-    block_num INT,
-    stairs_id CHAR(1),
-    floor_num INT,
-    door_num INT,
-    zip_code INT NOT NULL,
-    town_name VARCHAR(100) NOT NULL,
-    country VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_address PRIMARY KEY (address_id),
-    CONSTRAINT fk_address_customers FOREIGN KEY (address_id) REFERENCES customers(customer_id),
-    CONSTRAINT check_zip_code CHECK (zip_code >= 10000),
-    CONSTRAINT check_valid_ints CHECK (gateway_num > 0 AND block_num > 0 AND door_num > 0)
 );
 
 CREATE TABLE purchase_order(
--- tables/7_customer.sql
+++ tables/7_customer.sql
@@ -1,15 +1,12 @@
--- CREATE TABLE customer(
---     customer_id INT CHECK(customer_id >= 0) NOT NULL,
---     username CHAR(30),
---     reg_date CHAR(14),
---     reg_time CHAR(14),
---     user_password CHAR(15),
---     customer_name CHAR(35) NOT NULL,
---     customer_surname1 CHAR(30) NOT NULL,
---     customer_surname2 CHAR(30),
---     customer_email VARCHAR(60),
---     customer_phone_number CHAR(9),
---     CONSTRAINT pk_customer PRIMARY KEY(customer_id)
+-- CREATE TABLE customers(
+--     customer_id NUMBER CHECK(customer_id >= 40000) NOT NULL,
+--     delivery_address VARCHAR(100) NOT NULL,
+--     billing_id,
+--     registered CHAR(1),
+--     customer_email VARCHAR(100) NOT NULL,
+--     customer_phone_number INT CHECK(customer_phone_number >= 100000000),
+--     CONSTRAINT pk_customers PRIMARY KEY(customer_id),
+--     CONSTRAINT check_customer_phone_number CHECK(999999999 > customer_phone_number)
 -- );
 
 -- CREATE SEQUENCE TO GENERATE customer_id automatically
@@ -25,54 +22,34 @@ DROP TABLE temp_table;
 
 CREATE TABLE temp_table(
     customer_id NUMBER CHECK(customer_id >= 40000),
-    username CHAR(30),
-    reg_date CHAR(14),
-    reg_time CHAR(14),
-    user_password CHAR(15),
-    customer_name CHAR(35),
-    customer_surname1 CHAR(30),
-    customer_surname2 CHAR(30),
-    customer_email VARCHAR(60),
-    customer_phone_number CHAR(9)
+    delivery_address VARCHAR(1000) NOT NULL,
+    billing_id NUMBER,
+    registered CHAR(1),
+    customer_email VARCHAR(100),
+    customer_phone_number INT CHECK(customer_phone_number >= 100000000)
 );
 
-INSERT INTO temp_table(username ,reg_date,
-reg_time , user_password, customer_name, customer_surname1,
-customer_surname2, customer_email, customer_phone_number)
-SELECT
-    USERNAME,
-    REG_DATE,
-    REG_TIME,
-    USER_PASSW,
-    CLIENT_NAME,
-    CLIENT_SURN1,
-    CLIENT_SURN2,
+INSERT INTO temp_table (delivery_address, registered, customer_email, customer_phone_number)
+SELECT DISTINCT
+    COALESCE(DLIV_WAYTYPE, '') || ' ' || COALESCE(DLIV_WAYNAME, '') || ' ' || COALESCE(DLIV_GATE, '') || ' ' ||
+    COALESCE(DLIV_BLOCK, '') || ' ' || COALESCE(DLIV_STAIRW, '') || ' ' || COALESCE(DLIV_FLOOR, '') || ' ' ||
+    COALESCE(DLIV_DOOR, '') || ' ' || COALESCE(DLIV_ZIP, '') || ' ' || COALESCE(DLIV_TOWN, '') || ' ' ||
+    COALESCE(DLIV_COUNTRY, '') AS delivery_address,
+    CASE
+        WHEN USERNAME IS NOT NULL AND (CLIENT_EMAIL IS NOT NULL OR CLIENT_MOBILE IS NOT NULL) THEN 'Y'
+        ELSE 'N'
+    END AS registered,
     CLIENT_EMAIL,
     CLIENT_MOBILE
 FROM fsdb.trolley
-GROUP BY USERNAME, REG_DATE, REG_TIME, USER_PASSW, CLIENT_NAME,
-CLIENT_SURN1, CLIENT_SURN2, CLIENT_EMAIL, CLIENT_MOBILE;
+WHERE PAYMENT_TYPE IS NOT NULL AND DLIV_WAYNAME IS NOT NULL AND DLIV_FLOOR IS NOT NULL AND DLIV_DOOR IS NOT NULL AND 
+    DLIV_COUNTRY IS NOT NULL AND DLIV_TOWN IS NOT NULL AND (CLIENT_EMAIL IS NOT NULL OR CLIENT_MOBILE IS NOT NULL);
+
 
 UPDATE temp_table
 SET customer_id = (
     seq_customer_id.NEXTVAL
 );
-
-INSERT INTO customers(customer_id, username, reg_date, reg_time,
-user_password, customer_name, customer_surname1, customer_surname2,
-customer_email, customer_phone_number)
-SELECT
-customer_id,
-username,
-reg_date,
-reg_time,
-user_password,
-customer_name,
-customer_surname1,
-customer_surname2,
-customer_email,
-customer_phone_number
-FROM temp_table;
 
 DROP TABLE temp_table;
 
@@ -94,17 +71,17 @@ from fsdb.trolley WHERE USERNAME IS NULL;
 select distinct USERNAME, CLIENT_EMAIL, CLIENT_MOBILE, REG_DATE
 from fsdb.trolley WHERE USERNAME IS NULL;
 
-select distinct USERNAME, CLIENT_EMAIL from fsdb.trolley;
+select distinct USERNAME, CLIENT_EMAIL from fsdb.trolley
+WHERE username IS NOT NULL AND CLIENT_EMAIL IS NOT NULL;
+
 select distinct USERNAME, CLIENT_MOBILE from fsdb.trolley;
 
-select distinct USERNAME, CLIENT_MOBILE from fsdb.;
 
 
 -- address inputs
-SELECT distinct DLIV_DATE,DLIV_TIME,DLIV_WAYTYPE,DLIV_WAYNAME,DLIV_GATE,DLIV_BLOCK, DLIV_STAIRW, DLIV_FLOOR, DLIV_DOOR, DLIV_ZIP, DLIV_TOWN, DLIV_COUNTRY 
+SELECT distinct PAYMENT_TYPE, DLIV_DATE,DLIV_TIME,DLIV_WAYTYPE,DLIV_WAYNAME,DLIV_GATE,DLIV_BLOCK, DLIV_STAIRW, DLIV_FLOOR, DLIV_DOOR, DLIV_ZIP, DLIV_TOWN, DLIV_COUNTRY 
 from fsdb.trolley
-where DLIV_WAYNAME IS NOT NULL AND DLIV_FLOOR IS NOT NULL AND DLIV_DOOR IS NOT NULL AND DLIV_COUNTRY IS NOT NULL AND DLIV_TOWN IS NOT NULL;
+where PAYMENT_TYPE IS NOT NULL AND DLIV_WAYNAME IS NOT NULL AND DLIV_FLOOR IS NOT NULL AND DLIV_DOOR IS NOT NULL AND DLIV_COUNTRY IS NOT NULL AND DLIV_TOWN IS NOT NULL;
 
 select distinct text from fsdb.posts;
 desc fsdb.trolley;
-desc fsdb.posts;
