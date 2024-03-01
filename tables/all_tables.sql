92eed4551b27c331330c35c3e01d0eeb33fa93d6
Liang Ji Zhu <www.1159068228@gmail.com>
2024-03-01 02:43:06 +0100

    all_tables

--- tables/all_tables.sql
+++ tables/all_tables.sql
@@ -1,4 +1,6 @@
+-- 
 -- catalogue
+-- 
 -- The attributes used in fsdb.catalogue
 -- select PRODUCT, FORMAT, COFFEA, VARIETAL, ORIGIN, ROASTING, DECAF, PACKAGING from fsdb.catalogue;
 
@@ -8,9 +10,11 @@ SELECT DISTINCT
     c.PRODUCT
 FROM fsdb.catalogue c
 WHERE c.PRODUCT IS NOT NULL;
+
+-- 
+-- products
 -- 
 
--- products
 -- CREATE SEQUENCE TO GENERATE product_id automatically
 DROP SEQUENCE seq_product_id;
 
@@ -81,8 +85,9 @@ DROP TABLE temp_table;
 -- THERE ARE 750 possibilities of product with different product names NOT TAKING INTO ACCOUNT THE FORMAT AND PACKAGING
 
 -- 
-
 -- marketing_format
+-- 
+
 -- CREATE SEQUENCE TO GENERATE format_id automatically
 DROP SEQUENCE seq_format_id;
 
@@ -156,3 +161,134 @@ DROP TABLE temp_table;
 
 -- 
 -- p_reference
+-- 
+
+DROP TABLE temp_table;
+
+CREATE TABLE temp_table (
+    bar_code VARCHAR(15),
+    packaging VARCHAR(15),
+    retail_price VARCHAR(14),
+    product_id NUMBER,
+    product_name VARCHAR(50),
+    current_stock VARCHAR(5),
+    min_stock VARCHAR(5),
+    max_stock VARCHAR(5)
+);
+
+
+INSERT INTO temp_table(bar_code, packaging, retail_price, current_stock, product_name, min_stock, max_stock)
+SELECT
+    BARCODE,
+    PACKAGING,
+    RETAIL_PRICE,
+    CUR_STOCK,
+    PRODUCT,
+    MIN_STOCK,
+    MAX_STOCK
+FROM fsdb.catalogue
+GROUP BY BARCODE, PACKAGING, RETAIL_PRICE, CUR_STOCK, PRODUCT, MIN_STOCK, MAX_STOCK;
+
+
+UPDATE temp_table
+SET product_id = (
+    SELECT product_id
+    FROM products
+    WHERE product_name = temp_table.product_name
+);
+
+INSERT INTO p_reference(bar_code, product_id, packaging, retail_price, min_stock, max_stock, current_stock)
+SELECT
+    bar_code,
+    product_id,
+    packaging,
+    retail_price,
+    min_stock,
+    max_stock,
+    current_stock    
+FROM temp_table;
+
+DROP TABLE temp_table;
+
+-- select * from p_reference;
+-- select min_stock, max_stock, current_stock from p_reference;
+
+-- 
+-- replacement_order
+-- 
+
+-- CREATE SEQUENCE TO GENERATE format_id automatically
+DROP SEQUENCE seq_replacement_order_id;
+
+CREATE SEQUENCE seq_replacement_order_id
+START WITH 20000
+INCREMENT BY 1
+MAXVALUE 30000
+NOCYCLE;
+
+DROP TABLE temp_table;
+
+-- create a temporal table to work with the datas
+CREATE TABLE temp_table (
+    replacement_order_id NUMBER CHECK(replacement_order_id >= 20000) NOT NULL,
+    bar_code VARCHAR(15) NOT NULL,
+    supplier VARCHAR(35),
+    request_amount NUMBER CHECK(request_amount > 0) NOT NULL,
+    request_date DATE,
+    delivery_date DATE,
+    rorder_state VARCHAR(15),
+    received_date DATE,
+    payment VARCHAR(30) NOT NULL
+);
+
+INSERT INTO temp_table (replacement_order_id, bar_code, supplier, request_date, payment)
+SELECT
+    seq_replacement_order_id.NEXTVAL,
+    barcode,
+    supplier,
+    SYSDATE AS request_date,
+    PROV_BANKACC
+FROM
+    (SELECT DISTINCT c.barcode, c.supplier, c.PROV_BANKACC
+     FROM fsdb.catalogue c
+    --  check that bar_code from p_reference table matches with this table
+     JOIN p_reference s ON c.barcode = s.bar_code
+     WHERE c.barcode IS NOT NULL 
+       AND c.supplier IS NOT NULL 
+       AND c.PROV_BANKACC IS NOT NULL
+       AND TO_NUMBER(s.current_stock) < TO_NUMBER(s.min_stock)
+    );
+
+-- rorder_state
+UPDATE temp_table
+SET rorder_state = (
+    CASE 
+        WHEN SYSDATE > delivery_date THEN 'Placed'
+        WHEN received_date > SYSDATE THEN 'fulfilled'
+        ELSE 'Draft'
+    END
+);
+
+-- request_amount generated automatically??
+UPDATE temp_table tt
+SET tt.request_amount = (
+    SELECT (TO_NUMBER(pr.max_stock) - TO_NUMBER(pr.current_stock))
+    FROM p_reference pr
+    WHERE pr.bar_code = tt.bar_code 
+        AND TO_NUMBER(pr.current_stock) < TO_NUMBER(pr.min_stock)
+);
+
+-- upload to main table
+INSERT INTO replacement_order (replacement_order_id, bar_code, supplier, request_amount, request_date, rorder_state, received_date, payment)
+SELECT
+    replacement_order_id,
+    bar_code,
+    supplier,
+    request_amount,
+    request_date,
+    rorder_state,
+    received_date,
+    payment
+FROM TEMP_TABLE;
+
+DROP TABLE temp_table;
